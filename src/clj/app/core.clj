b41f7f2dedf0bd51a717d999f7a2bdbd71c4c8d9
Mobe <mobe@email.com>
2022-04-23 21:11:56 +0200

    Get route with path-params

--- src/clj/app/core.clj
+++ src/clj/app/core.clj
@@ -1,32 +1,64 @@
 (ns app.core
   (:gen-class)
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.defaults :as ring-defaults]
-            [reitit.ring :as ring]))
+            [ring.middleware.defaults :as rd]
+            [reitit.dev.pretty :as pretty]
+            [ring.util.http-response :as response]
+            [reitit.ring :as r]))
 
 
 (defonce server (atom nil))
 
 
-(defn handler [req]
-  {:status 200, :body "OK200"})
+(defn api-handler [req]
+  (prn req)
+  {:status 200 :body "OK"})
+
+
+(defn root-handler [req]
+  (prn req)
+  {:status 200 :body "index"})
+
+(defn home-handler [path-params]
+  (prn path-params)
+  {:status 200 :body "index"}
+  )
+
+(def routes
+  [[ "/"  {
+           :get root-handler}]
+   ["/api" {
+            :get api-handler}]
+   ["/home/:a/:b/:c" {:get
+                 (fn [{{:keys [a b c]} :path-params}]
+                   (response/ok (str "Value is: " a "/" b "/" c)))}]]
+  )
+
+(comment
+  
+   ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) {:a-map {:a 1 :b 2 :c 3}})
+
+   ((fn [{a :a b :b c :c}] (+ a b c)) {:a 1 :b 2 :c 3})
+  
+   (let [{a :a b :b c :c} {:a 1 :b 2 :c 3}] (+ a b c))
+
+   )
 
 
 (def router
-  (ring/router
-   [["/api" {:get handler}]
-    ["/test" {:get handler}]]))
+  (r/router routes
+    {:exception pretty/exception}))
 
 
-(def app (ring/ring-handler router))
+(def app (r/ring-handler router))
 
 
 (defn start-jetty! []
   (reset!
    server
-   (jetty/run-jetty (ring-defaults/wrap-defaults
+   (jetty/run-jetty (rd/wrap-defaults
                      #'app
-                     ring-defaults/site-defaults)
+                     rd/site-defaults)
                     {:join? false
                      :port 4000})))
  
@@ -36,7 +68,7 @@
   (reset! server nil))
 
 
-(defn -main [& args]
+(defn -main [& _]
   (println "Server started!")
   (start-jetty!))
 
@@ -53,9 +85,17 @@
 (comment
   (stop-jetty!))
 
+;; curl -X GET -d hello=world localhost:4000/api
+(comment
+  (restart-jetty!)
+  (app
+   {:request-method :get
+    :uri "/api"
+    :query-params {"x" "2"}}))
 
 (comment
   (restart-jetty!)
   (app
-   {:request-method :get, :uri "/api"}))
+   {:request-method :get
+    :uri "/home/1" }))
 
