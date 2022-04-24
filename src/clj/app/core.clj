b292639120b043ff87aafb1c7bd579fc0ddedf15
Mounir Benzid <m.benzid@douglas.de>
2022-04-24 11:03:52 +0200

    Replace lambda in route with defn

--- src/clj/app/core.clj
+++ src/clj/app/core.clj
@@ -3,7 +3,6 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :as rd]
             [reitit.dev.pretty :as pretty]
-            [ring.util.http-response :as response]
             [reitit.ring :as r]))
 
 
@@ -19,30 +18,38 @@
   (prn req)
   {:status 200 :body "index"})
 
-(defn home-handler [path-params]
-  (prn path-params)
-  {:status 200 :body "index"}
+
+(defn home [{{:keys [a b c]} :path-params}]
+  {:status 200 :body (str a b c)}
   )
+
 
 (def routes
-  [[ "/"  {
-           :get root-handler}]
-   ["/api" {
-            :get api-handler}]
-   ["/home/:a/:b/:c" {:get
-                 (fn [{{:keys [a b c]} :path-params}]
-                   (response/ok (str "Value is: " a "/" b "/" c)))}]]
-  )
+  [[ "/"  {:get root-handler}]
+   ["/api" {:get api-handler}]
+   ["/home/:a/:b/:c" {:get home}]] )
 
 (comment
-  
-   ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) {:a-map {:a 1 :b 2 :c 3}})
 
-   ((fn [{a :a b :b c :c}] (+ a b c)) {:a 1 :b 2 :c 3})
-  
-   (let [{a :a b :b c :c} {:a 1 :b 2 :c 3}] (+ a b c))
 
-   )
+  (def hais
+    (fn [{a :a b :b c :c}] (+ a b c)))
+
+  (hais {:a 1 :b 2 :c 3})
+
+
+  (defn a [req]
+    ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) req))
+
+  (a {:a-map {:a 1 :b 2 :c 3}})
+
+  ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) {:a-map {:a 1 :b 2 :c 3}})
+
+  ((fn [{a :a b :b c :c}] (+ a b c)) {:a 1 :b 2 :c 3})
+
+  (let [{a :a b :b c :c} {:a 1 :b 2 :c 3}] (+ a b c))
+
+  )
 
 
 (def router
