546950b1ca797f07853054568d67e0d191c063ef
Mounir Benzid <m.benzid@douglas.de>
2022-04-25 01:02:09 +0200

    In the beginning there was darkness

--- src/clj/app/core.clj
+++ src/clj/app/core.clj
@@ -1,60 +1,70 @@
 (ns app.core
   (:gen-class)
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.defaults :as rd]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [reitit.dev.pretty :as pretty]
+            [ring.util.response :refer [redirect]]
+            [ring.middleware.oauth2 :refer [wrap-oauth2]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [environ.core :refer [env]]
             [reitit.ring :as r]))
+
+
+(def config
+  {:github-client-id "e94c21eaeb852d237790"
+   :github-client-secret "d3e18d6edf8d85c37679c5c934d04892b45e37e9"})
 
 
 (defonce server (atom nil))
 
 
-(defn api-handler [req]
+(defn get-api [req]
   (prn req)
   {:status 200 :body "OK"})
 
 
-(defn root-handler [req]
+(defn get-root [req]
   (prn req)
   {:status 200 :body "index"})
 
 
-(defn home [{{:keys [a b c]} :path-params}]
-  {:status 200 :body (str a b c)}
-  )
+(defn get-login [_]
+  (prn (:github-auth-page config) )
+  {redirect(:github-auth-page config)})
+
+
+(defn get-home [{{:keys [a b c]} :path-params}]
+  {:status 200 :body (str a b c)})
 
 
 (def routes
-  [[ "/"  {:get root-handler}]
-   ["/api" {:get api-handler}]
-   ["/home/:a/:b/:c" {:get home}]] )
-
-(comment
-
-
-  (def hais
-    (fn [{a :a b :b c :c}] (+ a b c)))
-
-  (hais {:a 1 :b 2 :c 3})
+  [[ "/"  {:get get-root}]
+   ["/login" {:get get-login}]
+   ["/api" {:get get-api}]
+   ["/home/:a/:b/:c" {:get get-home}]] )
 
 
-  (defn a [req]
-    ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) req))
+(def github-spec
+  {:github {:authorize-uri    "https://github.com/login/oauth/authorize"
+            :access-token-uri "https://github.com/login/oauth/access_token"
+            :client-id        (:github-client-id config)
+            :client-secret    (:github-client-secret config)
+            :scopes           ["user:email"]
+            :launch-uri       "/oauth2/github"
+            :redirect-uri     "/oauth2/github/callback"
+            :landing-uri      "/landing"}})
 
-  (a {:a-map {:a 1 :b 2 :c 3}})
 
-  ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) {:a-map {:a 1 :b 2 :c 3}})
-
-  ((fn [{a :a b :b c :c}] (+ a b c)) {:a 1 :b 2 :c 3})
-
-  (let [{a :a b :b c :c} {:a 1 :b 2 :c 3}] (+ a b c))
-
-  )
+(def my-site-defaults
+  (-> site-defaults (assoc-in [:session :cookie-attrs :same-site] :lax)))
 
 
 (def router
-  (r/router routes
-    {:exception pretty/exception}))
+  (r/router (-> routes
+                (wrap-oauth2 github-spec)
+                (wrap-defaults my-site-defaults))
+            
+            {:exception pretty/exception}))
 
 
 (def app (r/ring-handler router))
@@ -63,9 +73,9 @@
 (defn start-jetty! []
   (reset!
    server
-   (jetty/run-jetty (rd/wrap-defaults
+   (jetty/run-jetty (wrap-defaults
                      #'app
-                     rd/site-defaults)
+                     site-defaults)
                     {:join? false
                      :port 4000})))
  
@@ -104,5 +114,25 @@
   (restart-jetty!)
   (app
    {:request-method :get
-    :uri "/home/1" }))
+    :uri "/login" }))
+
+
+(comment
+
+  (def hais
+    (fn [{a :a b :b c :c}] (+ a b c)))
+
+  (hais {:a 1 :b 2 :c 3})
+
+  (defn a [req]
+    ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) req))
+
+  (a {:a-map {:a 1 :b 2 :c 3}})
+
+  ((fn [{{a :a b :b c :c} :a-map}] (+ a b c)) {:a-map {:a 1 :b 2 :c 3}})
+
+  ((fn [{a :a b :b c :c}] (+ a b c)) {:a 1 :b 2 :c 3})
+
+  (let [{a :a b :b c :c} {:a 1 :b 2 :c 3}] (+ a b c)))
+
 
